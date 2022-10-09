6f42b40653e0cb2fe61ba1cf6f19f24ddc9da7b4
goblicpl <bartosz.miziura@o2.pl>
2022-10-09 14:23:38 +0200

    Pre-Setup of Firebase

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -10,6 +10,7 @@ module.exports = function (api) {
             "@/components": "./app/components",
             "@/screens": "./app/screens",
             "@/router": "./app/router",
+            "@/contexts": "./app/contexts",
             "@/theme": "./app/theme",
             "@/firebase": "./app/firebase/index",
             "@/assets": "./assets",
